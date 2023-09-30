d7a8197f0d8860a8cf8955abce4060ad137a3b7c
Sora Tonami <ms0503@outlook.com>
2023-10-01 04:59:03 +0900

    フォントの修正。CSS変数のファイル切り出し。目次のスタイルの修正。不要なCSS変数の削除。

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,13 @@
 'use strict';
 
 import Link from 'next/link';
+import { ArrowUpCircle, Box, FileEarmarkPlus, Tags } from 'react-bootstrap-icons';
 import { Blog, microCMSClient } from '@/lib/microcms-client';
-import { Stack } from '@/lib/client-react-bootstrap';
+import { Col, Container, Row, Stack } from '@/lib/client-react-bootstrap';
 import { iso2datetime } from '@/lib/time';
 import type { JSX } from 'react';
 import type { Metadata } from 'next';
+import styles from '@/app/blog/[id]/Blog.module.scss';
 
 export const metadata: Metadata = {
     description: 'STM32系・Web系の技術ブログ',
@@ -26,11 +28,21 @@ export default async function BlogHome(): Promise<JSX.Element> {
             <h1>記事一覧</h1>
             <Stack gap={2}>
                 {blog.map(post => (
-                    <div key={post.id}>
-                        <h2><Link href={`/blog/${post.id}`}>{post.title}</Link></h2>
-                        <span>{iso2datetime(post.createdAt)}</span>
-                        <span>{iso2datetime(post.updatedAt)}</span>
-                    </div>
+                    <Link href={`/blog/${post.id}`} key={post.id}>
+                        <Container className={styles['blog-entry']}>
+                            <Row>
+                                <Col as="h2">{post.title}</Col>
+                            </Row>
+                            <Row className={styles['blog-metadata']} lg={6}>
+                                <Col as="small" title="記事作成日時"><FileEarmarkPlus /> {iso2datetime(post.createdAt)}</Col>
+                                <Col as="small" title="最終更新日時"><ArrowUpCircle /> {iso2datetime(post.updatedAt)}</Col>
+                            </Row>
+                            <Row className={styles['blog-metadata']} lg={6}>
+                                <Col as="small" title="カテゴリー"><Box /> {post.category.name}</Col>
+                                <Col as="small" title="タグ"><Tags /> {post.tags.length === 0 ? 'なし' : post.tags.map(tag => tag.name).join(', ')}</Col>
+                            </Row>
+                        </Container>
+                    </Link>
                 ))}
             </Stack>
         </>
