d91f75a2f94ca569f23bc3238d0149acdf5bfe24
Sora Tonami <ms0503@outlook.com>
2024-04-20 01:04:30 +0900

    コピーライトの統合。

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,13 +1,14 @@
 'use strict';
 
 import styles from '@/app/components/Footer.module.scss';
+import { getCopyright } from '@/lib/info';
 import type { JSX } from 'react';
 
 export function Footer(): JSX.Element {
     return (
         <footer className={styles['footer']}>
             <div className={styles['copyright']}>
-                <small>Copyright &copy; 2023 Sora Tonami. All rights reserved.</small>
+                <small>{getCopyright()}</small>
             </div>
         </footer>
     );
